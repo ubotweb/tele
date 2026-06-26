a43f97b96cbe0aa74c6e6337edb425ce65e49676
ubotweb <ubot.web.id@gmail.com>
2026-06-26 17:32:48 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,28 @@
 import { defineConfig } from 'vite';
 import honox from 'honox/vite';
+import client from 'honox/vite/client';
 import adapter from '@hono/vite-dev-server/cloudflare';
 
-export default defineConfig({
-  plugins: [
-    honox({
-      devServer: {
-        adapter
-      }
-    })
-  ]
+export default defineConfig(({ mode }) => {
+  // Mode Klien: Membangun komponen interaktif frontend (Islands)
+  if (mode === 'client') {
+    return {
+      plugins: [client()]
+    };
+  }
+
+  // Mode Server (Bawaan): Membangun backend SSR tanpa mencari index.html
+  return {
+    build: {
+      emptyOutDir: false,
+      ssr: 'app/server.ts' // Mengarahkan Vite secara eksplisit ke file server
+    },
+    plugins: [
+      honox({
+        devServer: {
+          adapter
+        }
+      })
+    ]
+  };
 });
