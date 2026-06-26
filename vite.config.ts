b1eb3702e356c98111993bfc94a5579ca20ef82b
ubotweb <ubot.web.id@gmail.com>
2026-06-26 17:41:24 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,22 @@ import client from 'honox/vite/client';
 import adapter from '@hono/vite-dev-server/cloudflare';
 
 export default defineConfig(({ mode }) => {
-  // Mode Klien: Membangun komponen interaktif frontend (Islands)
   if (mode === 'client') {
     return {
       plugins: [client()]
     };
   }
 
-  // Mode Server (Bawaan): Membangun backend SSR tanpa mencari index.html
   return {
     build: {
-      emptyOutDir: false,
-      ssr: 'app/server.ts' // Mengarahkan Vite secara eksplisit ke file server
+      outDir: 'dist',
+      rollupOptions: {
+        output: {
+          // Memastikan output file SSR diberi nama _worker.js untuk Cloudflare Pages
+          entryFileNames: '_worker.js' 
+        }
+      },
+      ssr: 'app/server.ts'
     },
     plugins: [
       honox({
