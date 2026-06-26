34e92550ee2d6d1be80ba7669100194980061806
ubotweb <ubot.web.id@gmail.com>
2026-06-26 20:46:59 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import { Env } from './types/index';
 import { authMiddleware } from './middlewares/auth';
 import { subscriptionMiddleware } from './middlewares/subscription';
 
-// 1. Import SEMUA Controllers
 import { authApp } from './controllers/auth.controller';
 import { projectApp } from './controllers/project.controller';
 import { botApp } from './controllers/bot.controller';
@@ -16,28 +15,30 @@ import { productApp } from './controllers/product.controller';
 import { transactionApp } from './controllers/transaction.controller';
 import { orderApp } from './controllers/order.controller';
 import { cloudApp } from './controllers/cloud.controller';
+import { accountApp } from './controllers/account.controller';
 
 const apiApp = new Hono<{ Bindings: Env }>();
 
-// A. Rute Publik
+// Rute Publik
 apiApp.route('/auth', authApp);
-apiApp.route('/webhook', webhookApp); // Webhook Telegram tidak perlu login!
-apiApp.route('/orders', orderApp); // Pembuatan order & webhook QRIS
+apiApp.route('/webhook', webhookApp);
+apiApp.route('/orders', orderApp);
 
-// B. Rute Admin (Proteksi Super Admin di dalam controllernya)
+// Rute Super Admin
 apiApp.use('/admin/*', authMiddleware);
 apiApp.route('/admin', adminApp);
 
-// C. Rute Akun/Tenant Umum
+// Rute Level Akun (Tenant)
 apiApp.use('/projects/*', authMiddleware);
 apiApp.route('/projects', projectApp);
 
-// D. Rute Cloud (Bisa di level akun, karena 1 user mungkin punya 1 cloudary untuk semua project)
 apiApp.use('/cloud/*', authMiddleware);
 apiApp.route('/cloud', cloudApp);
 
-// E. Rute Level Project (Proteksi Langganan)
-// Middleware langganan memastikan user hanya bisa akses projectnya jika 'active'
+apiApp.use('/account/*', authMiddleware);
+apiApp.route('/account', accountApp);
+
+// Rute Level Project (Proteksi Langganan)
 apiApp.use('/projects/:project_id/bot/*', subscriptionMiddleware);
 apiApp.use('/projects/:project_id/categories/*', subscriptionMiddleware);
 apiApp.use('/projects/:project_id/banners/*', subscriptionMiddleware);
@@ -45,7 +46,6 @@ apiApp.use('/projects/:project_id/commands/*', subscriptionMiddleware);
 apiApp.use('/projects/:project_id/products/*', subscriptionMiddleware);
 apiApp.use('/projects/:project_id/transactions/*', subscriptionMiddleware);
 
-// F. Mounting Rute Spesifik Project
 apiApp.route('/projects/:project_id/bot', botApp);
 apiApp.route('/projects/:project_id/categories', categoryApp);
 apiApp.route('/projects/:project_id/banners', bannerApp);
